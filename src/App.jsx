39b92b6015d7807d759fc9b690b586cbf3706bb2
kmk9000 <kallekoivuniemi@gmail.com>
2025-12-10 19:26:44 +0200

    add pointer highlight, mui typography, styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 // import theme from "./theme.js";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div className="flex bg-blue-300 min-h-screen min-w-full">
+    <div className="relative flex min-h-screen min-w-full">
+      <div
+        className="pointer-events-none fixed inset-0 z-30 transition duration-300"
+        style={{
+          background: `radial-gradient(200px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+        }}
+      />
       <Header />
       <Main />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,26 @@
+import Typography from "@mui/material/Typography";
+
 export default function Header() {
   return (
-    <div className="flex flex-col bg-blue-600 text-white p-4 w-1/4">
-      <header className="bg-blue-600 text-white p-4">
-        <div>
-          <div>
-            <h1 className="text-3xl font-bold">
-              <a href="about">My Portfolio</a>
-            </h1>
-            <h2>Front End Developer</h2>
-          </div>
-          <p>Short description here</p>
-        </div>
-        <nav className="mt-2">
+    <div className="flex flex-col bg-blue-950 text-white p-12 w-1/4">
+      {/* bg-blue-600 */}
+      <header>
+        <Typography variant="h4" component="div" gutterBottom>
+          {/* <div className="font-bold text-3xl pb-4"> */}
+          <a href="#about">My Portfolio</a>
+          {/* </div> */}
+        </Typography>
+        <Typography variant="h6">Front End Developer</Typography>
+
+        <Typography variant="h7">Short description here</Typography>
+        <nav className="mt-4">
           <ul className="space-y-2">
             <li>
               <a
                 href="#about"
                 className="hover:underline flex items-center gap-2 group"
               >
-                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-40 duration-300"></div>
+                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-30 duration-300"></div>
                 <div className="flex items-center py-1">About</div>
               </a>
             </li>
@@ -27,7 +29,7 @@ export default function Header() {
                 href="#projects"
                 className="hover:underline flex items-center gap-2 group"
               >
-                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-40 duration-300"></div>
+                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-30 duration-300"></div>
                 <div className="flex items-center py-1">Projects</div>
               </a>
             </li>
@@ -36,14 +38,13 @@ export default function Header() {
                 href="#contact"
                 className="hover:underline flex items-center gap-2 group"
               >
-                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-40 duration-300"></div>
+                <div className="flex align-items-center h-1 w-20 bg-[#00b4d8] origin-left transform group-hover:w-30 duration-300"></div>
                 <div className="flex items-center py-1">Contact</div>
               </a>
             </li>
           </ul>
         </nav>
       </header>
-
       <ul className="mt-auto p-5">
         <li>Stuff</li>
       </ul>
