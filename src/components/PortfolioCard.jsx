8c027c768960fa35dfc37ea3d89a28661c132aad
kmk9000 <kallekoivuniemi@gmail.com>
2026-03-06 12:14:03 +0200

    add breathing glow effect to PortfolioCard

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function PortfolioCard({ children }) {
   return (
-    <div className="rounded-xl border border-white/5 bg-black/20 p-6 shadow-2xl">
+    <div className="portfolio-card-glow rounded-xl border border-white/5 bg-black/20 p-6 shadow-2xl">
       {children}
     </div>
   );
